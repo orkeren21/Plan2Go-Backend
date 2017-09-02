0c525f5cd14ef2a93050368dff1d264486d61b59
Or Keren <dude2110@gmail.com>
2017-09-02 11:11:59 +0300

    Slight Revision of Database structure
    Specifically revision of Plans table. Plans table has a list of events associated with it (with some summary information) so that retrieval is fast.
    Based on de-normalization design pattern of noSQL Databases

--- EventsRouter.js
+++ EventsRouter.js
@@ -16,15 +16,47 @@ router.post('/addEvent', function(req, res){
         Item: req.body
     };
 
+    //When we add an event, we need to update the current plan, and add event params to it
+    //Meaning, addEvent needs planID on request,
+    //TODO: Need to rethink keys for events, looks like events need their own EventID
+    var planID = parseInt(req.body.PlanID);
+    var planParams = {
+        TableName: "Plans",
+        Key: {
+            "PlanID": planID
+        },
+        UpdateExpression: "SET EventsList = list_append(EventsList, :p)",
+        ExpressionAttributeValues:{
+            ":p": [
+                {
+                    "EventDate": req.body.EventDate,
+                    "StartTime": req.body.StartTime,
+                    "Duration": req.body.Duration,
+                    "AttractionName": "test"
+                }
+            ]
+        }
+    };
+
     docClient.put(params, function(err, data){
         if (err) {
             console.error("Error while adding data: " + JSON.stringify(err));
-            res.send("Error while adding data: " + JSON.stringify(err));
+            //res.send("Error while adding data: " + JSON.stringify(err));
         } else {
             console.log("Successfully added data.");
-            res.send("Successfully added data");
+            //res.send("Successfully added data");
         }
     });
+
+    docClient.update(planParams, function(err, data){
+        if (err) {
+            console.error("Error while updating data: " + JSON.stringify(err));
+            res.send("Error while updating data: " + JSON.stringify(err));
+        } else {
+            console.log("Successfully updated data.");
+            res.send("Successfully updated data");
+        }
+    })
 });
 
 router.delete('/deleteEvent', function(req, res){
